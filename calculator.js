c1d2a8e3c932a033e502e0df917aacfb7a828fdf
Syed Hamail Mohi Uddin Qazi <hamailsyed139@gmail.com>
2024-04-27 18:26:41 +0500

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,36 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+const answers = await inquirer.prompt([
+    { type: "number",
+        name: "numberOne",
+        message: "What is your first number?"
+    },
+    { type: "number",
+        name: "numberTwo",
+        message: "What is your second number?"
+    },
+    { type: "list",
+        name: "operator",
+        message: "What operator would you like to use?",
+        choices: ["+", "-", "*", "/"] }
+]);
+const { numberOne, numberTwo, operator } = answers;
+let result;
+switch (operator) {
+    case "+":
+        result = numberOne + numberTwo;
+        break;
+    case "-":
+        result = numberOne - numberTwo;
+        break;
+    case "*":
+        result = numberOne * numberTwo;
+        break;
+    case "/":
+        result = numberOne / numberTwo;
+        break;
+    default:
+        console.log("Invalid operator");
+}
+console.log(chalk.green(`The result of ${numberOne} ${operator} ${numberTwo} is ${result}`));
